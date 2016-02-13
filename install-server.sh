322bb1edfac16fc2cb0e935d55a879d385fdc8d9
Alin Nica <alin.nica@lendico.de>
2016-02-13 23:00:50 +0100

    stuff

---
+++ install-server.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+echo "deb https://download.go.cd /" | sudo tee /etc/apt/sources.list.d/gocd.list
+curl https://download.go.cd/GOCD-GPG-KEY.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install go-server
+sudo apt-get install git
