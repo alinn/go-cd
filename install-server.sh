8ea0438496b08fe1a714508b0fb3c1738911361d
Alin Nica <alin.nica@lendico.de>
2016-02-14 02:27:59 +0100

    more stuff

--- install-server.sh
+++ install-server.sh
@@ -2,5 +2,5 @@
 echo "deb https://download.go.cd /" | sudo tee /etc/apt/sources.list.d/gocd.list
 curl https://download.go.cd/GOCD-GPG-KEY.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install go-server
-sudo apt-get install git
+sudo apt-get install puppet
+sudo gem install librarian-puppet
